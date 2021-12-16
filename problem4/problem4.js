6e7adcdd0f01a6b138c9d2762842993eaa36c49a
Farand Albert Sujatmiko <albertfarand@gmail.com>
2021-12-16 23:49:08 +0700

    second commit with ss

--- problem4/problem4.js
+++ problem4/problem4.js
@@ -1,33 +1,12 @@
 function munculSekali(angka) {
-  let int = Array.from(angka).map(Number);
-  let int_sort = int.sort();
+  const int = Array.from(angka);
   let uniques = [];
-  let uniques_sort = [];
+  const intedit = int;
 
-  for (i = 0; i < int_sort.length; i++) {
-    if (i == 0) {
-      if (int_sort[i] != int_sort[i + 1]) {
-        uniques.push(int_sort[i]);
-      } 
-    } else if (i == int_sort.length-1) {
-      if (int_sort[i] != int_sort[i - 1]) {
-        uniques.push(int_sort[i]);
-      }
-    } else {
-      if (int_sort[i] != int_sort[i + 1] && int_sort[i] != int_sort[i - 1]) {
-        uniques.push(int_sort[i]);
-      }
-    }
+  for (i = 0; i < int.length; i++) {
+    let int_copy = intedit.splice(intedit.indexOf(intedit[i]));
   }
-  for (i = 0; i < angka.length; i++) {
-    if (uniques.includes(angka[i])) {
-      continue;
-    } else {
-      uniques_sort.push(angka[i]);
-      uniques.splice(uniques.indexOf(angka[i]));
-    }
-  }
-  return (int_sort[0] == int_sort[1]);
+  return intedit;
 }
 console.log(munculSekali("76523752"));
 module.exports = munculSekali;
